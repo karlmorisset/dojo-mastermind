7b6e36d02a00f84c2550192405947d2aaed77af9
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-12-06 22:41:51 +0100

    add readme

--- MasterMindTest.php
+++ MasterMindTest.php
@@ -11,6 +11,7 @@ class MasterMindTest extends TestCase
     public function testGetScore()
     {
         $mastermind = new MasterMind();
+        $this->assertEquals([0,0], $mastermind->getScore(['purple','orange','pink','yellow']));
         $this->assertEquals([2,0], $mastermind->getScore(['red','blue','green','pink']));
         $this->assertEquals([0,1], $mastermind->getScore(['blue','orange','pink','yellow']));
         $this->assertEquals([2,1], $mastermind->getScore(['red','blue','green','black']));
