43b3004c67dda76d5e87045445bdaea214d558db
Karl Morisset <karl.morisset.register@gmail.com>
2021-12-17 12:27:01 +0100

    base ok

--- MasterMind.php
+++ MasterMind.php
@@ -4,13 +4,6 @@ class MasterMind
 {
     public function getScore(array $masterColors, array $playerColors)
     {
-        $scores = ["in" => 0, "good" => 0];
 
-        for ($i = 0; $i < count($masterColors); $i++) {
-            if (in_array($playerColors[$i], $masterColors)) $scores['in']++;
-            if ($playerColors[$i] === $masterColors[$i]) $scores["good"]++;
-        }
-
-        return [$scores['in'] - $scores["good"], $scores["good"]];
     }
 }
